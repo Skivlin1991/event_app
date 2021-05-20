bb0fb2f22f85357f02447ed9081a8d3a948cb592
SKVLIN1991 <s_kivlin@hotmail.co.uk>
2021-05-20 14:42:02 +0100

    first commit

---
+++ events_app/controllers/__init__.py
@@ -0,0 +1,20 @@
+from flask import runder_template, request, redirect
+from app import app
+from app.modules.event_list import events_list, create_new_event
+from app.modules.event import Event
+
+@app.route('/events')
+def index():
+    return render_template('index.html',title = 'Events', events =events_list)
+
+@app.route('/events', methods=['POST'])
+def create_event():
+    eventName = request.form['event_name']
+    eventDate = request.form['date']
+    eventDescription = request.form['description']
+    eventGuests = request.form['number_of_guests']
+    eventRoom = request.form['room_location']
+    eventRecurring = request.form['recurring']
+    newEvent = Event(eventDate, eventName, eventGuests, eventRoom, eventDescription, eventRecurring)
+    create_new_event(newEvent)
+    return redirect('/events')
